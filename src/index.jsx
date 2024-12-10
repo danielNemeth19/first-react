4aa916fb06038701a716748378f2a315647ef3c6
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-12-10 23:10:11 +0100

    (feat) React fact card

--- src/index.jsx
+++ src/index.jsx
@@ -2,52 +2,23 @@ import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
 
-function MyAwesomeNavbar() {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">MyAwesomeNavbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarsExample03">
-                    <ul className="navbar-nav me-auto mb-2 mb-sm-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form role="search">
-                        <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                    </form>
-                </div>
-            </div>
-        </nav>
+function ReactFactsCard() {
+    return(
+        <main>
+            <img src="vite.svg" width="40px" alt="Vite logo"/>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200k starts on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
-function MainContent(){
-    return <h1>Blaa</h1>
-}
-
 root.render(
-    <div>
-        <MyAwesomeNavbar />
-        <MainContent />
-    </div>
+    <ReactFactsCard />
 )
 
