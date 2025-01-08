02f804aebe16ed5f6a4cbbcaecc128886d3e29a8
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-01-09 00:19:36 +0100

    (feat) Completes form examples

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -19,10 +19,21 @@ export default function App() {
         const password = formData.get("password")
         const description = formData.get("description")
         const employmentStatus = formData.get("employmentStatus")
+        const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+        const favColor = formData.get("favColor")
         console.log(email)
         console.log(password)
         console.log(description)
         console.log(employmentStatus)
+        console.log(dietaryRestrictions)
+        console.log(favColor)
+
+        const data = Object.fromEntries(formData)
+        const allData = {
+            ...data,
+            dietaryRestrictions
+        }
+        console.log(allData)
     }
 
     return (
@@ -42,19 +53,19 @@ export default function App() {
             <br />
             <h1>React 19 Signup form</h1>
             <form action={signUp}>
-                <label htmlFor="email">Email:</label>
-                <input id="email" type="email" name="email" placeholder="blaa@hfz.com" />
+                <label htmlFor="email2">Email:</label>
+                <input id="email2" type="email" name="email" placeholder="blaa@hfz.com" />
                 <br />
-                <label htmlFor="password">Password:</label>
-                <input id="password" type="password" name="password" />
+                <label htmlFor="password2">Password:</label>
+                <input id="password2" type="password" name="password" />
                 <br />
-                <label htmlFor="description">Description</label>
-                <textarea id="description" name="description"></textarea>
+                <label htmlFor="description2">Description</label>
+                <textarea id="description2" name="description"></textarea>
 
                 <fieldset>
                     <legend>Employment Status</legend>
                     <label>
-                        <input type="radio" name="employmentStatus" value="unemployed"/>
+                        <input type="radio" name="employmentStatus" value="unemployed" />
                         Unemployed
                     </label>
                     <label>
@@ -66,6 +77,30 @@ export default function App() {
                         Full-time
                     </label>
                 </fieldset>
+
+                <fieldset>
+                    <legend>Dietary Restrictions:</legend>
+                    <label>
+                        <input type="checkbox" name="dietaryRestrictions" value="kosher" />
+                        Kosher
+                    </label>
+                    <label>
+                        <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="vegan" />
+                        Vegan
+                    </label>
+                    <label>
+                        <input type="checkbox" name="dietaryRestrictions" defaultChecked={true} value="gluten-free" />
+                        Gluten-free
+                    </label>
+                </fieldset>
+
+                <label htmlFor="favColor">What is your favourite color?</label>
+                <select id="favColor" name="favColor" defaultValue="" required>
+                    <option value="">-- Choose favorite color --</option>
+                    <option value="red">Red</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="blue">Blue</option>
+                </select>
 
                 <button>Submit</button>
             </form>
