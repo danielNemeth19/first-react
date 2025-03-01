fc4819de6e319ff9611ab26a395ac38050ccb15a
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-03-01 17:51:49 +0100

    (feat) Starts Tenzies capstones project

---
+++ src/AppTenzies.jsx
@@ -0,0 +1,9 @@
+import "./index_tenzies.css"
+
+
+export default function AppTenzies() {
+    return (
+        <main>
+        </main>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client"
-import App from "./AppChefClaude.jsx"
+import App from "./AppTenzies.jsx"
 
 const root = createRoot(document.getElementById("root"))
 root.render(<App />)
