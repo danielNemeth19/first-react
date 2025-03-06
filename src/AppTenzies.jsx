e7c7fe519295e8a82bbac84f26e24e44698c6b97
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-03-06 23:55:25 +0100

    (feat) Creates basic design for the page

--- src/AppTenzies.jsx
+++ src/AppTenzies.jsx
@@ -1,9 +1,22 @@
 import "./index_tenzies.css"
+import Die from "./components/Die.jsx"
 
 
 export default function AppTenzies() {
     return (
         <main>
+            <div className="container">
+                <Die value={1} />
+                <Die value={3} />
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={2} />
+                <Die value={5} />
+            </div>
         </main>
     )
 }
