22161642f51f872616b315b724d6e9594201ce11
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-03-10 23:47:31 +0100

    (feat) Adds button to roll dice

--- src/AppTenzies.jsx
+++ src/AppTenzies.jsx
@@ -3,14 +3,16 @@ import "./index_tenzies.css"
 import Die from "./components/Die.jsx"
 
 
-
-
 export default function AppTenzies() {
     const [dice, setDice] = useState(generateAllNewDice())
 
     function generateAllNewDice() {
         const max = 6;
         return new Array(10).fill(0).map(() => Math.ceil(Math.random() * max))
+    }
+
+    function rollDice(){
+        setDice(generateAllNewDice())
     }
 
     const diceElements = dice.map((item, i) => <Die key={i} value={item} />)
@@ -20,6 +22,7 @@ export default function AppTenzies() {
             <div className="dice-container">
                 {diceElements}
             </div>
+            <button className="roll" onClick={rollDice}>Roll</button>
         </main>
     )
 }
