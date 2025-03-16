b579c9b27d18059d4ea42fec4bae13a595aedc97
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-03-16 23:57:06 +0100

    (feat) Starts with Assembly: endgame

---
+++ src/AppAssembly.jsx
@@ -0,0 +1,12 @@
+import "./index_assembly.css"
+
+export default function AppAssembly() {
+    return (
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word in under 8 attempts to keep the programming world save from Assembly!</p>
+            </header>
+        </main>
+    )
+}
