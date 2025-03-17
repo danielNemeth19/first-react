de4aaf00e8136d34a8dc7a2312caf29d2a63c8e2
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-03-17 23:28:54 +0100

    (feat) Adds status section (hardcoded for now)

--- src/AppAssembly.jsx
+++ src/AppAssembly.jsx
@@ -7,6 +7,12 @@ export default function AppAssembly() {
                 <h1>Assembly: Endgame</h1>
                 <p>Guess the word in under 8 attempts to keep the programming world save from Assembly!</p>
             </header>
+            <main>
+                <section className="game-status">
+                  <h2>You win!</h2>
+                  <p>Well done! 🎉</p>
+                </section>
+            </main>
         </main>
     )
 }
