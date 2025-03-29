61e4497a24212d64599b7d966bff02f824cc7cfb
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-03-29 23:56:05 +0100

    (feat) Implements state for storing letters

--- src/AppAssembly.jsx
+++ src/AppAssembly.jsx
@@ -4,9 +4,9 @@ import { languages } from "./languages.js"
 
 export default function AppAssembly() {
     const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([])
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
-
 
     const languaeElements = languages.map((language) => {
         const styles = {
@@ -16,35 +16,42 @@ export default function AppAssembly() {
         return <span key={language.name} className="chip" style={styles}>{language.name}</span>
     })
 
-    const letterElements = currentWord.split("").map((char, idx)=>{
+    const letterElements = currentWord.split("").map((char, idx) => {
         return <span key={idx}>{char.toUpperCase()}</span>
     })
 
-    const keyboard = alphabet.split("").map((char) => {
-        return <button key={char}>{char}</button>
-    })
+    function addGuessedLetter(char) {
+        setGuessedLetters(prev => {
+            return prev.includes(char) ? prev : [...prev, char]
+        })
+    }
 
-    return (
-        <main>
-            <header>
-                <h1>Assembly: Endgame</h1>
-                <p>Guess the word in under 8 attempts to keep the programming world save from Assembly!</p>
-            </header>
+    const keyboardElements = alphabet.split("").map((char) => {
+                return <button onClick={() => addGuessedLetter(char)} key={char}>{char.toUpperCase()}</button>
+            })
+
+        return (
             <main>
-                <section className="game-status">
-                    <h2>You win!</h2>
-                    <p>Well done! 🎉</p>
-                </section>
-                <section className="language-chips">
-                    {languaeElements}
-                </section>
-                <section className="word">
-                    {letterElements}
-                </section>
-                <section className="keyboard">
-                    {keyboard}
-                </section>
+                <header>
+                    <h1>Assembly: Endgame</h1>
+                    <p>Guess the word in under 8 attempts to keep the programming world save from Assembly!</p>
+                </header>
+                <main>
+                    <section className="game-status">
+                        <h2>You win!</h2>
+                        <p>Well done! 🎉</p>
+                    </section>
+                    <section className="language-chips">
+                        {languaeElements}
+                    </section>
+                    <section className="word">
+                        {letterElements}
+                    </section>
+                    <section className="keyboard">
+                        {keyboardElements}
+                    </section>
+                    <button className="new-game">New Game</button>
+                </main>
             </main>
-        </main>
-    )
-}
+        )
+    }
